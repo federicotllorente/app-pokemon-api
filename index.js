4342bff97b85d5a8e2609a031ed38cd4f48538fb
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-15 18:37:26 +0100

    Image links updated (now are uploaded in Dropbox) and Webpack problems fixed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import router from "./routes/index";
 
+import Styles from './styles/main.css';
+
 window.addEventListener("load", router);
 window.addEventListener("hashchange", router);
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,22 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            },
         ]
     },
     // Used plugins

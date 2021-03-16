30c6865e614650fe31d9661b309ac42422f5d244
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-03-16 09:59:55 +0100

    Favicon and image links updated

--- templates/Header.js
+++ templates/Header.js
@@ -2,11 +2,11 @@ const Header = () => {
     const view = `
         <div class="title">
             <a href="/">
-                <img src="https://previews.dropbox.com/p/thumb/ABGHtqxkFfXT34e2rVsnReBg6BYoYg36Rm0adOVSQzAiQ5qOs8wdh_xxtGpELS7MGqgw8DmjUSJsrktcV4FpNw4DB6sNRknQvBmvKEkLUKHMmA9n16yN9_clcelNXKgTOhGfamm4eW8-bZh_wK5cfXOyaTPjijjsE9q5vYXpv-FUl5_YmDybbgJGrnvBK11ifmXYbvYkv7R0x1W2FYiofASprV6Q1UlhHto1YwO2vHdD9qrO8jICH0C6KLqwSqMYOskgRA2qOxEtslEQWvvGDbFth55vardnTqcs6_ZnabhjriT7AZxWzlgl5bZY-ocE8Sej2NBtDiwOKEIgQr-EaJa5vaBhM8idAkzYe0RGe5BUUg/p.png?fv_content=true&size_mode=5">
+                <img src="https://federicotllorente.com/wp-content/uploads/2021/03/return_arrow.png">
             </a>
             <h2>Pokédex</h2>
         </div>
-        <p>by <a href="https://github.com/federicotllorente">Federico Tejedor Llorente</a></p>
+        <p>by <a href="https://github.com/federicotllorente" target="_blank" rel="noreferrer">Federico Tejedor Llorente</a></p>
     `;
     return view;
 };
